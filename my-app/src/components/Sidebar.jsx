dd843a9f440d1fd28b058f200181b5b72c8c3d23
Hyeree Park <hp2240@nyu.edu>
2022-07-25 16:48:18 -0400

    working on landing page(displaying products from my db)

---
+++ my-app/src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div className="mene-hover-fill">
+      <ul>
+        <li className="sidebar-element">
+          <a>OUTER</a>
+        </li>
+        <li className="sidebar-element">
+          <a>TOP</a>
+        </li>
+        <li className="sidebar-element">
+          <a>BOTTOM</a>
+        </li>
+        <li className="sidebar-element">
+          <a>DRESS</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,2 +1,28 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+
+const Home = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await axios.get(`http://localhost:3001/api/products`)
+      console.log(res.data.products)
+      setProducts(res.data.products)
+    }
+    getProducts()
+  }, [])
+
+  return (
+    <div>
+      {/* {products ? <img src={products[0].image} /> : ''} */}
+      <div className="container">
+        {products.map((product) => (
+          <img src={product.image} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Home
