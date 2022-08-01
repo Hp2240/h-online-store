30e160fd783c964b3bed6574090d66458929b8ff
Hyeree Park <hp2240@nyu.edu>
2022-08-01 12:27:39 -0400

    exclude .env

--- client/src/pages/Outer.js
+++ client/src/pages/Outer.js
@@ -7,31 +7,27 @@ const Outer = (props) => {
   const [productsCategory, setProductsCategory] = useState([])
   const [outer, setOuter] = useState(null)
 
-  // useEffect(() => {
-  //   const getProductByCategory = async () => {
-  //     const res = await axios.get(`http://localhost:3001/api/products`)
-  //     console.log(res.data.products)
-  //     SetProductsCategory(res.data.products)
-  //   }
-  //   getProductByCategory()
-  // }, [])
-
-  // console.log(props.products, 'this is product from home')
-
-  // let filtered = productsCategory.find((i) => i.category === 'outer')
-  // if (filtered) {
-  //   setOuter(filtered)
-  // }
   useEffect(() => {
-    let filtered = props.products.find(
-      (product) => product.category === 'outer'
-    )
-    setProductsCategory(filtered)
+    const getProductByCategory = async () => {
+      const res = await axios.get(`http://localhost:3001/api/products`)
+      console.log(res.data.products)
+      setProductsCategory(res.data.products)
+    }
+    getProductByCategory()
   }, [])
 
-  return <div></div>
+  let filtered = productsCategory.find((i) => i.category === 'outer')
+  if (filtered) {
+    setOuter(filtered)
+  }
+  // useEffect(() => {
+  //   let filtered = props.products.find(
+  //     (product) => product.category === 'outer'
+  //   )
+  //   setProductsCategory(filtered)
+  // }, [])
 
-  // return <div> {outer ? <img src={productsCategory.image} /> : ''} </div>
+  return <div> {outer ? <img src={filtered.image} /> : ''} </div>
 }
 
 export default Outer
